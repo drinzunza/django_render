db9cc2b05fd9c422564125648cafbb650d16f8aa
Sergio Inzunza <inxunxa@gmail.com>
2026-05-25 13:21:31 -0700

    remove settings

--- config/settings.py
+++ config/settings.py
@@ -13,14 +13,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
-import environ
-
-# Environment variables
-env = environ.Env(
-    DEBUG=(bool, False),
-)
-environ.Env.read_env(BASE_DIR / '.env')
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
